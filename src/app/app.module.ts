6e0718960b557d6c536d554c8e5b97b621bdaa02
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2018-03-14 15:17:43 +0200

    Last changes Lv-293

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { MediaMatcher } from '@angular/cdk/layout';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './app-material.module';
 import { HighlightPipe } from './highlight.pipe';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightPipe
+    HighlightPipe,
+    StudentDetailComponent
   ],
   imports: [
     AppMaterialModule,
